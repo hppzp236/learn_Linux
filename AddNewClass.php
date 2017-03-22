25b2ba624085b55253c64b9c53a69d1e480eae42
hppzp236 <hpp236@gmail.com>
2017-03-22 12:38:10 +0800

    添加了一个新类

---
+++ AddNewClass.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouning
+ * Date: 2017/3/22
+ * Time: 12:31
+ */
+
+namespace newClass;
+
+
+class AddNewClass
+{
+     public function addClass(){
+         echo "添加一个新的类来测试PHPstorm";
+     }
+}
